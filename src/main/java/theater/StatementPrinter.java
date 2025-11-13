630c5ed65666787be11734b0faae9df5357bbe5a
Aiden <aiden.chettleburgh720@gmail.com>
2025-11-13 14:24:55 -0500

    Inlined variable and updated log.txt - Completed Task 2.1

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -32,8 +32,6 @@ public class StatementPrinter {
         final NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
         for (Performance performance : getInvoice().getPerformances()) {
-            final int amount = getAmount(performance);
-
             // add volume credits
             volumeCredits += Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
             // add extra credit for every five comedy attendees
@@ -44,9 +42,9 @@ public class StatementPrinter {
             // print line for this order
             result.append(String.format("  %s: %s (%s seats)%n",
                     getPlay(performance).getName(),
-                    frmt.format(amount / Constants.PERCENT_FACTOR),
+                    frmt.format(getAmount(performance) / Constants.PERCENT_FACTOR),
                     performance.getAudience()));
-            totalAmount += amount;
+            totalAmount += getAmount(performance);
         }
         result.append(String.format("Amount owed is %s%n", frmt.format(totalAmount / Constants.PERCENT_FACTOR)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
